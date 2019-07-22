1aebd7dff1e162ed1fd250759bcfbeba7b3c8591
Horytnik <Qh33yphsGit>
2019-07-22 22:47:50 +0200

    First working

--- PermMissingElem.py
+++ PermMissingElem.py
@@ -1,17 +1,28 @@
 A = [2,3,1,5]
+B = []
+C = [1]
+D = [2,1,3,4,5,7]
+E = [2,1,3,4,5,6]
+F = [2,7,3,4,5,6]
+G = [1,2]
 
-def Factorial(FacNum):
-    if FacNum == 0:
-        return 1
-    else:
-        return FacNum + Factorial(FacNum-1)
+def allElmSum(n):
+    sum = 0
+    for a in range(1,n+1):
+        sum = sum + a
+    return sum
+
 
 def solution(A):
     ElementsSum = sum(A)
-    SumOfArray = Factorial(len(A))
-    return (ElementsSum - SumOfArray - 1)
-
-print(solution(A))
+    SumOfArray = allElmSum(len(A)+1)
+    return (SumOfArray - ElementsSum)
 
 
-print(Factorial(len(A)))
+print('Sol',solution(A))
+print('Sol',solution(B))
+print('Sol',solution(C))
+print('Sol',solution(D))
+print('Sol',solution(E))
+print('Sol',solution(F))
+print('Sol',solution(G))
