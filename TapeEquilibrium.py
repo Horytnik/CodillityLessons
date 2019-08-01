db078c636137189a1ec4a4726d2c02d0027e1203
Horytnik <Qh33yphsGit>
2019-08-01 20:49:57 +0200

    Correct version

--- TapeEquilibrium.py
+++ TapeEquilibrium.py
@@ -1,16 +1,11 @@
+import sys
+
 A = [3, 1, 2, 4, 3]
 
 def solution(n):
-    print(n)
-    SortedArray = sorted(n)
-    oldAbsDiff = 100000
-    for a in range(0, len(SortedArray)-1):
-        FirstSum = sum(SortedArray[0:a+1])
-        SecondSum = sum(SortedArray[a+1:len(SortedArray)])
-        newAbsDiff = abs(FirstSum - SecondSum)
-        if newAbsDiff < oldAbsDiff:
-            minAbs = newAbsDiff
-        oldAbsDiff = abs(FirstSum - SecondSum)
+    minAbs = sys.maxsize
+    for a in range(0, len(n)-1):
+        minAbs = min(minAbs, abs(sum(n[0:a+1]) - sum(n[a+1:len(n)])))
     return minAbs
 
 print(solution(A))
