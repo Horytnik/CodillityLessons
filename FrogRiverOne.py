4415c5fad3d82ff72d305db6d25c7e8ce4258b18
Horytnik <Qh33yphsGit>
2019-08-04 17:40:32 +0200

    First 100% solution

--- FrogRiverOne.py
+++ FrogRiverOne.py
@@ -2,15 +2,17 @@ InpArray = [1,3,1,4,2,3,5,4]
 Pos = 5
 
 def solution(X,A):
-    maxInd = 0
     if len(set(A)) == X:
-        for SetElm in set(A):
-            maxInd = max(maxInd, A.index(SetElm))
-        return maxInd
+        leafpositions = [0]*X
+        amountoffields = X
+        for index in range(0, len(A)):
+            if leafpositions[A[index]-1] == 0:
+                leafpositions[A[index]-1] = 1
+                amountoffields -= 1
+                if amountoffields == 0:
+                    return index
     else:
         return -1
-
-
 
 print(solution(Pos, InpArray))
 
